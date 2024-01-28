fac960db12517d86ddd10c4ae42c1a64196c2c9a
amanda <156504099+mandawh@users.noreply.github.com>
2024-01-28 06:21:40 -0700

    Update game.js

--- game.js
+++ game.js
@@ -1,3 +1,17 @@
 function myFunction() {
   document.getElementById("sample").innerHTML = ""
 }
+    var x = 2
+    var y = 4
+    
+    var sum = x+y              
+
+    if (sum = 6){
+      document.write("you win")
+    }
+    else if(sum >6){
+      document.write("you lose")
+    }           
+    else{
+      document.write("you lose")
+    }
