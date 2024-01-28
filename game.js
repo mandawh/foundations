5a905eb76d57b7961ae1cd3d36474536c1d331be
amanda <156504099+mandawh@users.noreply.github.com>
2024-01-28 07:15:36 -0700

    Update game.js

--- game.js
+++ game.js
@@ -2,3 +2,19 @@ function myFunction() {
   document.getElementById("sample").innerHTML = "you win"
 }
 
+<script language = "javascript" type="text/javascript">
+    var x = 2
+    var y = 4
+    
+    var sum = x+y              
+
+    if (sum = 6){
+      document.write("you win")
+    }
+    else if(sum >6){
+      document.write("you lose")
+    }           
+    else{
+      document.write("you lose")
+    }
+</script>
