521934d8bf98219b925424a99c519c474f538013
amanda <156504099+mandawh@users.noreply.github.com>
2024-02-04 05:36:53 -0700

    Update strings.js

--- strings.js
+++ strings.js
@@ -1,11 +1,13 @@
 functon validateForm(){
   const palindromes = document.getElementById('palindromes').value;
-
+///using the form that is linked to sting.html-->
   if(palindromes != Palindromes){
     alert("this is not a Palindromes try again");
     return;
+    ///will alert when the incorrect word is submitted-->
   }
 
   alert("This is a Palindromes");
   document.getElementById('registrationForm').submit();
+  ///wil alert when the correct word is submitted-->
 }
